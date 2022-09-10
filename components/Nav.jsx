35b70126e5d248a5ad158b9ea08d9ce16a65cc46
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-10 14:57:15 -0600

    Adding a sidebar/drawer

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Nav() {
   return (
@@ -27,7 +29,15 @@ function Nav() {
               <li className="ml-10 text-sm uppercase">Contact</li>
             </Link>
           </ul>
+          <div className="md:hidden">
+            <MenuIcon fontSize="large" />
+          </div>
         </div>
+      </div>
+      {/* bg-black/70 where 70 isn the opactity value */}
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        {/* our actual Menu */}
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-400"></div>
       </div>
     </div>
   );
