52fd719164ead1a090ce3f4a720b4426ff29ac8c
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-10 14:10:55 -0600

    Creating Meta component

---
+++ components/Meta.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+function Meta({title, keywords, description}) {
+  return (
+    <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta name='keywords' content={keywords} />
+            <meta name='description' content={description} />
+            <meta charSet='utf-8' />
+            <link rel='icon' href='/favicon.ico' />
+            <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+    title:'WebDev News',
+    keywords:'webdev, web development, web development news, web development articles',
+    description:'WebDev News is a web development news website. We share web development news and articles.'
+}
+
+export default Meta
