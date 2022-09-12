28e65f04cda694b74b2ac415225fbfd558dda5a0
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-12 14:32:14 -0600

    About Me page

---
+++ components/About.jsx
@@ -0,0 +1,51 @@
+import Meta from "./Meta";
+import Image from "next/image";
+
+function About() {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <Meta title="About" />
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <h2>A bit about myself</h2>
+          <p>
+            Hi! My name is Bibek and I enjoy developing products that provide
+            intuitive and better user experience. My interest in web development
+            started back when I first customized my own custom profiles in
+            Tumblr. Although I have scrapped that account dude to embarrassment
+            but making the profile look the way I want it to make me realize I
+            enjoy creating websites that look pleasing.
+          </p>
+
+          <p>
+            Today, I am equipped with an arsenal (Fun fact: my favorite soccer
+            team) of technologies that allow me to create modern and clean
+            looking websites. My primary focus on any project is user experience
+            – making it as easy as possible to the user to achieve their desired
+            result with the website.
+          </p>
+          <p>
+            I specialize in full stack development using MERN tech stack along
+            with other technologies such as Next.js, GraphQ and Tailwind (Fun
+            fact: taught myself all these technologies). So I can work from
+            designing stunning UI at the front end using React or Next along
+            with Tailwind to designing a database at the backend using MongoDB
+            and designing endpoints using REST or creating queries using
+            GraphQL.
+          </p>
+        </div>
+        <div>
+          <Image
+            src="/../public/assets/portrait.jpg"
+            width={400}
+            height={300}
+            alt="Bibek"
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import Meta from "../components/Meta";
 import Nav from "../components/Nav";
+import About from "../components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Meta title="Bibek | Full Stack Developer" />
       <Nav />
       <Hero />
+      <About />
     </div>
   );
 }
