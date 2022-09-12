8e59cd7e8e3745a2edc18c107e6aef095ccd4585
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-12 14:56:02 -0600

    Finishing up the About Me page

--- components/About.jsx
+++ components/About.jsx
@@ -7,8 +7,8 @@ function About() {
       <Meta title="About" />
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <h2>A bit about myself</h2>
-          <p>
+          <h2 className="py-4 tracking-wider">A bit about myself</h2>
+          <p className="py-2 text-gray-600">
             Hi! My name is Bibek and I enjoy developing products that provide
             intuitive and better user experience. My interest in web development
             started back when I first customized my own custom profiles in
@@ -17,14 +17,14 @@ function About() {
             enjoy creating websites that look pleasing.
           </p>
 
-          <p>
+          <p className="py-2 text-gray-600">
             Today, I am equipped with an arsenal (Fun fact: my favorite soccer
             team) of technologies that allow me to create modern and clean
             looking websites. My primary focus on any project is user experience
             – making it as easy as possible to the user to achieve their desired
             result with the website.
           </p>
-          <p>
+          <p className="py-2 text-gray-600">
             I specialize in full stack development using MERN tech stack along
             with other technologies such as Next.js, GraphQ and Tailwind (Fun
             fact: taught myself all these technologies). So I can work from
@@ -34,13 +34,13 @@ function About() {
             GraphQL.
           </p>
         </div>
-        <div>
+        <div className=" w-full h-auto m-auto rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image
+            className="rounded-xl"
             src="/../public/assets/portrait.jpg"
-            width={400}
-            height={300}
             alt="Bibek"
-            layout="responsive"
+            width={600}
+            height={400}
           />
         </div>
       </div>
