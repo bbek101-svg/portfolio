dd86c2920e0a724816ec4f6c0c4bf14e5a001a2f
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-13 12:04:06 -0600

    Finishing the Projects section

--- components/ProjectRow.jsx
+++ components/ProjectRow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-function ProjectRow({ title, projImage, projectUrl }) {
+function ProjectRow({ title, projImage, projectUrl, tags }) {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709DFF]">
       <Image
@@ -14,9 +14,9 @@ function ProjectRow({ title, projImage, projectUrl }) {
         <h3 className="text-2xl text-white tracking-widest text-center">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">
-          React, Mongo, Express, Node, Stripe, GraphQL
-        </p>
+        {tags.map((tag) => (
+          <p className="pb-4 pt-2 text-white text-center">{tag}</p>
+        ))}
         <Link href={projectUrl}>
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 text-lg cusror-pointer">
             Details
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
 import whaleSucculent from "../public/assets/projects/homepage.png";
+import portfolio from "../public/assets/projects/portfolio.png";
 import Link from "next/link";
 import ProjectRow from "./ProjectRow";
 
 function Projects() {
+  const projects = [
+    {
+      title: "Whale Succulent E-commerce Website",
+      projImage: whaleSucculent,
+      projectUrl: "/whale",
+      tags: ["React", "Mongo", "Express", "Node", "Stripe", "GraphQL"],
+    },
+    {
+      title: "Portfolio",
+      projImage: portfolio,
+      projectUrl: "/portfolio",
+      tags: ["Next", "Tailwind"],
+    },
+  ];
   return (
     <div className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
@@ -12,11 +27,14 @@ function Projects() {
         </p>
         <h2 className="py-4">A showcase of my works</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectRow
-            title="Whale Succulent e-commerce website"
-            projImage={whaleSucculent}
-            projectUrl="/whale"
-          />
+          {projects.map((project) => (
+            <ProjectRow
+              title={project.title}
+              projImage={project.projImage}
+              projectUrl={project.projectUrl}
+              tags={project.tags}
+            />
+          ))}
         </div>
       </div>
     </div>
