809dae62bb7dec334af796da108d24c3e62dcc0a
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-13 11:01:47 -0600

    Finishing up the skills section

---
+++ components/SkillRow.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+function SkillRow({ skill }) {
+  return (
+    <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+      <div className="grid grid-cols-2 gap-4 justify-center items-center">
+        <div className="m-auto">
+          <Image
+            alt="react"
+            src={`/../public/assets/skills/${skill}.png`}
+            width="64px"
+            height="64px"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h4 className="uppercase">{skill}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SkillRow;
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,29 +1,58 @@
 import Image from "next/image";
 import React from "react";
+import SkillRow from "./SkillRow";
 
 function Skills() {
+  const frontend = ["next", "react", "tailwind-css", "sass", "javascript"];
+  const backend = ["express", "graphql", "nodejs"];
+  const database = ["mongodb", "mysql"];
+  const programming = ["c", "java", "python"];
+  const tools = ["github", "jira"];
+
   return (
-    <div className="w-full lg:h-screen p-2">
+    <div className="w-full lg:h-full p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+        <p className="py-4 text-xl tracking-widest uppercase text-[#5651e5]">
           My Arsenal
         </p>
-        <h2 className="py-4">Technologies</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  alt="react"
-                  src="/../public/assets/skills/react.png"
-                  width="64px"
-                  height="64px"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>React</h3>
-              </div>
-            </div>
+        <div className="py-4 flex flex-col justify-center">
+          <h2 className="py-4 tracking-wider">Frontend</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {frontend.map((skill) => (
+              <SkillRow skill={skill} />
+            ))}
+          </div>
+        </div>
+        <div className=" py-4 flex flex-col justify-center">
+          <h2 className="py-4 tracking-wider">Backend</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {backend.map((skill) => (
+              <SkillRow skill={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="py-4 flex flex-col justify-center">
+          <h2 className="py-4 tracking-wider">Database</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {database.map((skill) => (
+              <SkillRow skill={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="py-4 flex flex-col justify-center">
+          <h2 className="py-4 tracking-wider">Programming Languages</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {programming.map((skill) => (
+              <SkillRow skill={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="py-4 flex flex-col justify-center">
+          <h2 className="py-4 tracking-wider">Management Tools</h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {tools.map((skill) => (
+              <SkillRow skill={skill} />
+            ))}
           </div>
         </div>
       </div>
