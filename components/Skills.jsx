e9270073a6db749b06e18f619a69a9ee19a579e6
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-13 18:01:53 -0600

    Adding the ids

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -10,7 +10,7 @@ function Skills() {
   const tools = ["github", "jira"];
 
   return (
-    <div className="w-full lg:h-full p-2">
+    <div id="skills" className="w-full lg:h-full p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <p className="py-4 text-xl tracking-widest uppercase text-[#5651e5]">
           My Arsenal
