a0c4dfe04de8f4d4ce8f6287e5d0da4234f29393
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-09-17 14:15:56 -0600

    fix:typo

--- components/SkillRow.jsx
+++ components/SkillRow.jsx
@@ -6,7 +6,7 @@ function SkillRow({ skill }) {
       <div className="grid grid-cols-2 gap-4 justify-center items-center">
         <div className="m-auto">
           <Image
-            alt={`${skills}`}
+            alt={`${skill}`}
             src={`/../public/assets/skills/${skill}.png`}
             width="64px"
             height="64px"
