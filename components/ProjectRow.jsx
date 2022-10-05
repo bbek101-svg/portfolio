fa2d976e070502a5b4686a552d701e676974f5ff
Bibek Regmi <84989535+bbek101-svg@users.noreply.github.com>
2022-10-05 11:21:48 -0600

    fix: dark mode projects

--- components/ProjectRow.jsx
+++ components/ProjectRow.jsx
@@ -12,7 +12,7 @@ function ProjectRow({ title, projImage, projectUrl, tags }) {
         viewport={{ once: true, amount: 1 }}
         transition={{ duration: 0.5, staggerChildren: 0.5 }}
         whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-        className="cursor-pointer relative flex items-center hover:my-4 justify-center h-auto w-full shadow-xl dark:shadow-[#002A2e] shadow-gray-400 rounded-xl p-4 group hover:bg-[#001B1D]"
+        className="cursor-pointer relative flex items-center hover:my-4 justify-center h-auto w-full shadow-xl dark:hover:bg-[#A456F0] dark:shadow-[#002A2e] shadow-gray-400 rounded-xl p-4 group hover:bg-[#001B1D]"
       >
         <Image
           className="rounded-xl group-hover:opacity-10"
@@ -20,13 +20,13 @@ function ProjectRow({ title, projImage, projectUrl, tags }) {
           alt="/"
         />
         <motion.div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <motion.h3 className="mb-4 lg:text-xl text-sm text-white tracking-widest text-center">
+          <motion.h3 className="mb-4 lg:text-xl text-sm dark:text-[#fdfdfd] text-white tracking-widest text-center">
             {title}
           </motion.h3>
           {tags.map((tag, index) => (
             <motion.p
               key={index}
-              className="p-2 text-gray-700 lg:p-2 text-center inline bg-white rounded-xl m-4"
+              className="p-2 text-gray-700 lg:p-2 text-center inline dark:bg-[#002a2e] dark:text-[#FDFDFD] bg-white rounded-xl m-4"
             >
               {tag}
               {(index + 1) % 3 == 0 ? <br className="mb-4 mt-4" /> : ""}
